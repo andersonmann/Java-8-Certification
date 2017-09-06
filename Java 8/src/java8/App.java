9f8b347e221e3ff65ed49879569c1ffc626b7059
am55053 <am55053@sep000524.saqueepague.local>
2017-09-06 20:26:16 -0300

    ajustes

---
+++ Java 8/src/java8/App.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package java8;
+
+/**
+ * @author am55053
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("teste");
+	}
+
+}
